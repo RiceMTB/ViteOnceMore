6de587cf70bea1f8fff4e2f033c79df3acbeb99d
RiceMTB <reginald.h.rice.civ@mail.mil>
2023-07-21 05:12:09 -0500

    modified:   viteMe/package-lock.json 	modified:   viteMe/package.json 	modified:   viteMe/src/App.vue

--- viteMe/src/App.vue
+++ viteMe/src/App.vue
@@ -11,7 +11,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="REggie" />
+  <HelloWorld msg="Cool Guy" />
 </template>
 
 <style scoped>
